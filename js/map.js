91f5c6024fb1d4dbbb9b31f73b443ac1a5324957
zzokku <93818329+zzokku@users.noreply.github.com>
2022-11-28 14:21:28 +0200

    different file for map

---
+++ js/map.js
@@ -0,0 +1,9 @@
+stadi = [60.192059, 24.945831]
+var map = L.map('map').setView([60.192059, 24.945831], 10);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker(stadi).addTo(map);
+marker.bindPopup("<b>Helsingfors</b>").openPopup();
