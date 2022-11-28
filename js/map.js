28e595bd82193bbec56f178c3ce66aadd11c89fa
zzokku <93818329+zzokku@users.noreply.github.com>
2022-11-28 20:29:24 +0200

    map fix

--- js/map.js
+++ js/map.js
@@ -1,10 +1,31 @@
 // Longitude & Latitude of Helsinki
-stadi = [60.192059, 24.945831]
-var map = L.map('map').setView([60.192059, 24.945831], 10);
+helsinki_coordinates = [60.192059, 24.945831]
+
+
+// Map creation and zooming to Helsinki
+let map = L.map('map').setView([60.192059, 24.945831], 10);
+let layerGroup = L.layerGroup()
+map.addLayer(layerGroup)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 maxZoom: 19,
 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker(stadi).addTo(map);
-marker.bindPopup("<b>Helsingfors</b>").openPopup();
+
+// Adding search results to map
+addToMap = (json) => {
+    // Coordinates and name
+    coordinates = [json.location.lat, json.location.lon]
+    restName = json.name.fi
+    // Marker
+    marker = L.marker(coordinates).bindPopup(`<b>${restName}</b>`);
+    // Adding marker to layer group
+    layerGroup.addLayer(marker)
+}
+    
+
+// Marker reset
+resetMap = () => {
+    layerGroup.clearLayers();
+}
+
