ae6ca27834f294889998f382308c67a74b2a9910
zzokku <93818329+zzokku@users.noreply.github.com>
2022-11-28 15:19:26 +0200

    api call logged & folder name changed

--- js/api.js
+++ js/api.js
@@ -1,11 +1,12 @@
-apiHaku = () => {
-    query = document.getElementById("haku").value
-    fetch('https://api.tvmaze.com/search/shows?q=' + query)                         
-    .then(function(vastaus){        
-        return vastaus.json();        
-    }).then(function(json){         
-        hakuTulos(json);              
+apiSearch = (id) => {
+    apiCall = 'https://api.allorigins.win/get?url= ' + encodeURIComponent('https://open-api.myhelsinki.fi/v2/place/' + id)
+    fetch(apiCall)                    
+    .then(function(response){        
+        return response.json();        
+    }).then(function(json){
+        console.log(JSON.parse(json.contents))          
     }).catch(function(error){      
         console.log(error);           
     });
 }
+apiSearch(7)
