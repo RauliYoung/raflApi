6ef195a2742b13b23555d9a8c4d63d39634f0324
joonas <joonas.jouttijarvi@gmail.com>
2022-12-02 03:45:37 +0200

    geolocation fix

--- js/map.js
+++ js/map.js
@@ -3,33 +3,6 @@
 // Longitude & Latitude of Helsinki
 let helsinki_coordinates = [60.192059, 24.945831]
 
-const currentLocation=[]
-let options
-
-options= {
-    enableHighAccuracy: true
-
-}
-navigator.geolocation.watchPosition(onSuccess,onError,options)
-
-function onError() {
-    console.log("failed to get location")
-}
-function onSuccess (position) {
-    const {
-        latitude,
-        longitude
-    } = position.coords;
-    currentLocation.push(latitude,longitude);
-}
-
-const nothing = () => {
-
-currentLocation=[latitude,longitude]
-
-}
-console.log(currentLocation)
-
 // Map creation and zooming to Helsinki
 let map = new Map(helsinki_coordinates);
 map.createMap()
--- js/places.js
+++ js/places.js
@@ -1,37 +1,46 @@
+
+navigator.geolocation.getCurrentPosition(
+    (position) => {
+        const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+        };
+
 //location, search radius and type
-const request = {
-    location: new google.maps.LatLng(60.1556279,24.9367831,13),
-    radius: 500,
-    type: ['restaurant']
-};
+        const request = {
+            location: new google.maps.LatLng(pos),
+            radius: 1000,
+            type: ['restaurant']
+        };
 
-const results = [];
-const places = document.getElementById('places');
-const service = new google.maps.places.PlacesService(places);
+        const results = [];
+        const places = document.getElementById('places');
+        const service = new google.maps.places.PlacesService(places);
 
 
-const callback = (response, status, pagination) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        const callback = (response, status, pagination) => {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
 
-        results.push(...response);
+                results.push(...response);
 
-    }
+            }
 
-    if (pagination.hasNextPage) {
-        setTimeout(() => pagination.nextPage(), 2000);
-    } else {
-        displayResults();
-    }
-}
+            if (pagination.hasNextPage) {
+                setTimeout(() => pagination.nextPage(), 2000);
+            } else {
+                displayResults();
+            }
+        }
 //rating from best
-const displayResults = () => {
-    results.filter(result => result.rating)
-        .sort((a, b) => a.rating > b.rating ? -1 : 1)
-        .forEach(result => {
-            places.innerHTML += `<li>${result.name} - ${result.rating}</li>`;
-        });
+        const displayResults = () => {
+            results.filter(result => result.rating)
+                .sort((a, b) => a.rating > b.rating ? -1 : 1)
+                .forEach(result => {
+                    places.innerHTML += `<li>${result.name} - ${result.rating}</li>`;
+                });
 
-}
+        }
 
-service.nearbySearch(request, callback);
+        service.nearbySearch(request, callback);
+    })
 
