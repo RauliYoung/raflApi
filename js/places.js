e2e92a326f7a8667314da9c91d6cac295bf19614
joonas <joonas.jouttijarvi@gmail.com>
2022-12-06 15:35:12 +0200

    del places.html added top10 functionality.

--- js/places.js
+++ js/places.js
@@ -1,46 +1,42 @@
+const top10 = () => {
+    navigator.geolocation.getCurrentPosition(
 
-navigator.geolocation.getCurrentPosition(
-    (position) => {
-        const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-        };
+        (position) => {
+            const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            };
+            //location, search radius and type
 
-//location, search radius and type
-        const request = {
-            location: new google.maps.LatLng(pos),
-            radius: 500,
-            type: ['restaurant']
-        };
+            const request = {
+                location: new google.maps.LatLng(pos),
+                radius: 500,
 
-        const results = [];
-        const places = document.getElementById('places');
-        const service = new google.maps.places.PlacesService(places);
+                type: ['restaurant']
+            };
 
+            const results = [];
+            const places = document.getElementById('places');
+            const service = new google.maps.places.PlacesService(places);
 
-        const callback = (response, status, pagination) => {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-
-                results.push(...response);
-
+            const callback = (response, status) => {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    results.push(...response);
+                    displayResults();
+                }
             }
-
-            if (pagination.hasNextPage) {
-                setTimeout(() => pagination.nextPage(), 2000);
-            } else {
-                displayResults();
+            service.nearbySearch(request, callback);
+            //rating from best
+            const displayResults = () => {
+                results.filter(result => result.rating)
+                    .sort((a, b) => a.rating > b.rating ? -1 : 1)
+                    .slice(0, 10)
+                    .forEach(result => {//todo add markers to map
+                    });
             }
-        }
-//rating from best
-        const displayResults = () => {
-            results.filter(result => result.rating)
-                .sort((a, b) => a.rating > b.rating ? -1 : 1)
-                .forEach(result => {
-                    places.innerHTML += `<li>${result.name} - ${result.rating}</li>`;
-                });
+        });
+}
 
-        }
 
-        service.nearbySearch(request, callback);
-    })
+
 
