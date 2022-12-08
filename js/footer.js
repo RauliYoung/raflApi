f4bcb9a22e003e8ba8fc63cdaed7fb4a0b69a4df
RauliYoung <tristan.ellenberg@gmail.com>
2022-12-08 17:37:31 +0200

    jotain paskeea

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+const div = document.createElement("div");
+const jalka = document.createTextNode(
+  "<footer><h2>© Legitimate Code 4 Lyfe//Sandels group</h2></footer>"
+);
+div.appendChild(jalka);
+body.innerHTML += div;
