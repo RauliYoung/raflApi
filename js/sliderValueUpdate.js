9a1b8df36a12b7b8733bdf5c73e6100f8da6203f
RauliYoung <tristan.ellenberg@gmail.com>
2022-12-10 16:55:58 +0200

    fixattu slider ja kartta ja jne

--- js/sliderValueUpdate.js
+++ js/sliderValueUpdate.js
@@ -1,12 +1,10 @@
 let slider = document.getElementById("myRange");
 let output = document.getElementById("sliderVal");
 let range;
-output.innerHTML = slider.value;
+output.innerHTML = slider.value + " m";
 
-
-
-slider.oninput = function() {
-    output.innerHTML = this.value;
-    range = this.value;
-    map.editCircle(range)
-}
+slider.oninput = function () {
+  output.innerHTML = this.value + " m";
+  range = this.value;
+  map.editCircle(range);
+};
