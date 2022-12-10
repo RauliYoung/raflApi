c1cdc4824369d8b0534ca9b2fa4b8fb32b5aa0a7
RauliYoung <tristan.ellenberg@gmail.com>
2022-12-10 16:02:02 +0200

    fixes

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,6 @@
-const body = document.querySelector("body");
-const div = document.createElement("div");
-const jalka = document.createTextNode(
-  "<footer><h2>© Legitimate Code 4 Lyfe//Sandels group</h2></footer>"
-);
-div.appendChild(jalka);
-body.innerHTML += div;
+const createFooter = () => {
+  const body = document.querySelector("body");
+  const jalka = `<footer><h2>© Legitimate Code 4 Lyfe//Sandels group</h2></footer>`;
+
+  body.innerHTML += jalka;
+};
