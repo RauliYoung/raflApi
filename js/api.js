284240969b170097fe0f37c6713365b78b89e9b2
RauliYoung <tristan.ellenberg@gmail.com>
2022-12-13 18:46:54 +0200

    more info page added

--- js/api.js
+++ js/api.js
@@ -10,7 +10,7 @@ const idQuery = (id) => {
       return response.json();
     })
     .then(function (json) {
-      getData(json);
+      tulostaConsoliin(json);
     })
     .catch(function (error) {
       console.log(error);
@@ -18,79 +18,87 @@ const idQuery = (id) => {
 };
 
 const nameSearch = (object, name) => {
-  console.log(object)
+  console.log(object);
 
-  for (let obj of object){
-    const restName = object.name.fi
-        .toLowerCase()
-        .replace(/[^a-zA-Z0-9 ]/g, "");
-      if (restName.includes(name.toLowerCase())) {
-        // Lisää tulos...
-        //addrestMarker(object.id, object.name.fi, [objekt.location.lat, objekt.location.lon])
-      }
+  for (let obj of object) {
+    const restName = object.name.fi.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
+    if (restName.includes(name.toLowerCase())) {
+      // Lisää tulos...
+      //addrestMarker(object.id, object.name.fi, [objekt.location.lat, objekt.location.lon])
+    }
   }
-  console.log(object)
-  
-}
-
+  console.log(object);
+};
 
 // THE HAKU
 
 const query = (name, tags) => {
   //deleteList()
   let loc = false;
-  if (checked){
-    console.log("sijainti mukaan!")
+  if (checked) {
+    console.log("sijainti mukaan!");
     loc = true;
   }
   const ravintolat = localStorage.getItem("ravintolaOliot");
-  const ravintolaOliot2 = JSON.parse(ravintolat)
-  console.log(ravintolaOliot2)
+  const ravintolaOliot2 = JSON.parse(ravintolat);
+  console.log(ravintolaOliot2);
 
-  map.resetMap()
-  loadScreen()
+  map.resetMap();
+  loadScreen();
 
-try {
-
-  for (const objekt of ravintolaOliot2) {
-      const restName = objekt.name.fi.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, "");
-      let objtags = objekt.tags.map(tag => {return tag.name})
+  try {
+    for (const objekt of ravintolaOliot2) {
+      const restName = objekt.name.fi
+        .toLowerCase()
+        .replace(/[^a-zA-Z0-9 ]/g, "");
+      let objtags = objekt.tags.map((tag) => {
+        return tag.name;
+      });
       let tagit = true;
 
       // Tarkistetaan kuuluuko annetut tagit ravintolan tageihin.
       if (tags.length > 1) {
-        for (let i of tags){
-          if (!objtags.includes(i)){
-            tagit = false
-          } 
+        for (let i of tags) {
+          if (!objtags.includes(i)) {
+            tagit = false;
+          }
         }
       }
       if (restName.includes(name.toLowerCase()) && tagit) {
         // Jos haku sijainnin perusteella?
         if (loc) {
           // Otetaan myöhemmin arvo sliderista
-          if (haversineFormula(latlong, [objekt.location.lat, objekt.location.lon]) < range){
-            map.addMarker(objekt.id, objekt.name.fi, [objekt.location.lat, objekt.location.lon])
+          if (
+            haversineFormula(latlong, [
+              objekt.location.lat,
+              objekt.location.lon,
+            ]) < range
+          ) {
+            map.addMarker(objekt.id, objekt.name.fi, [
+              objekt.location.lat,
+              objekt.location.lon,
+            ]);
             //createListItem(objekt.id, objekt.name.fi, [objekt.location.lat, objekt.location.lon])
-          } 
-      } else {
-        map.addMarker(objekt.id, objekt.name.fi, [objekt.location.lat, objekt.location.lon])
-        //createListItem(objekt.id, objekt.name.fi, [objekt.location.lat, objekt.location.lon])
+          }
+        } else {
+          map.addMarker(objekt.id, objekt.name.fi, [
+            objekt.location.lat,
+            objekt.location.lon,
+          ]);
+          //createListItem(objekt.id, objekt.name.fi, [objekt.location.lat, objekt.location.lon])
+        }
       }
+    }
+    //Loading stops
+    setTimeout(loadScreenFinished, 2000);
+  } catch (error) {
+    console.log("haussa tapahtui virhe: " + error);
   }
-}
- //Loading stops
- setTimeout(loadScreenFinished, 2000)
-
-
-} catch (error) {
-  console.log("haussa tapahtui virhe: " + error)
-}
 };
 
 // CALLED WHEN SEARCH BUTTON CLICKED
 const search = () => {
-    //deleteList()
-    const queryValue = document.getElementsByClassName("searchField")[0].value;
-    query(queryValue, selectedBtn());
-}
+  //deleteList()
+  const queryValue = document.getElementsByClassName("searchField")[0].value;
+  query(queryValue, selectedBtn());
+};
