c51a6a91158faafb3631b9c4690c17845c0bb0d8
RauliYoung <tristan.ellenberg@gmail.com>
2022-12-14 10:19:47 +0200

    muutettu funktion nimi

--- js/api.js
+++ js/api.js
@@ -10,7 +10,7 @@ const idQuery = (id) => {
       return response.json();
     })
     .then(function (json) {
-      tulostaConsoliin(json);
+      tulostaRavintolanTiedotJaSijainti(json);
     })
     .catch(function (error) {
       console.log(error);
