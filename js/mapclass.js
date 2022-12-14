2eb0b76da12f4f6d24e54e2320e246005434f2ec
RauliYoung <tristan.ellenberg@gmail.com>
2022-12-14 10:10:25 +0200

    sisennetty koodia

--- js/mapclass.js
+++ js/mapclass.js
@@ -1,94 +1,102 @@
 class Map {
+  // Intial zoom location
+  location;
 
-    // Intial zoom location
-    location;
+  //Map to be initialized
+  map;
 
-    //Map to be initialized
-    map;
+  //Layer to be initialized
+  layerGroup;
 
-    //Layer to be initialized
-    layerGroup;
+  //Circle to be initialized
+  circle;
 
-    //Circle to be initialized
-    circle;
+  //Marker to current location
+  lmarker;
 
-    //Marker to current location
-    lmarker;
+  //Location layerGroup having all layers related to showing location
+  locGroup;
 
-    //Location layerGroup having all layers related to showing location
-    locGroup
+  // Constructor which takes only 'map zoom location'
+  constructor(location) {
+    this.location = location;
+  }
 
-    // Constructor which takes only 'map zoom location'
-    constructor (location){
-        this.location = location
-    }
+  // Method to create map
+  createMap(id) {
+    this.map = L.map(id).setView(this.location, 10);
+    this.layerGroup = L.layerGroup();
+    this.locGroup = L.layerGroup();
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map);
+    this.map.addLayer(this.layerGroup);
+    this.map.addLayer(this.locGroup);
+  }
 
-    // Method to create map
-    createMap(id){
-        this.map = L.map(id).setView(this.location, 10);
-        this.layerGroup = L.layerGroup()
-        this.locGroup = L.layerGroup()
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(this.map);
-        this.map.addLayer(this.layerGroup)
-        this.map.addLayer(this.locGroup)
-    }
+  // Resets map from markers
+  resetMap() {
+    this.layerGroup.clearLayers();
+  }
 
-    // Resets map from markers
-    resetMap(){
-        this.layerGroup.clearLayers();
-    }
-
-    // Add marker to map with bindPopup
-    addMarker(id, name, latlong){
-        L.marker(latlong).bindPopup(
+  // Add marker to map with bindPopup
+  addMarker(id, name, latlong) {
+    L.marker(latlong)
+      .bindPopup(
         `
             <b>${name}</b><br>
             <a href='resinfo.html?id=${id}'>Lisää tietoja ravintolasta<a/>`
-        ).addTo(this.layerGroup) 
-    }
+      )
+      .addTo(this.layerGroup);
+  }
 
-    addLocMarker(latlong){
-        this.lmarker = L.marker(latlong).bindPopup(`<b>Täällä</b>`
-        ).addTo(this.locGroup).openPopup()
-    }
+  addLocMarker(latlong) {
+    this.lmarker = L.marker(latlong)
+      .bindPopup(`<b>Täällä</b>`)
+      .addTo(this.locGroup)
+      .openPopup();
+  }
 
-    addRMarker(latlong, viesti){
-        this.lmarker = L.marker(latlong).bindPopup(`<b>${viesti}</b>`
-        ).addTo(this.map).openPopup()
-    }
-    // Creates circle with given radius
-    createCircle(latlong, radius){
-        this.circle = L.circle(latlong, 1000, {
-            color: "#216477", fillOpacity: 0.5, weight: 1
-        }).addTo(this.map);
-        //addTo(this.map)
-    }
+  addRMarker(latlong, viesti) {
+    this.lmarker = L.marker(latlong)
+      .bindPopup(`<b>${viesti}</b>`)
+      .addTo(this.map)
+      .openPopup();
+  }
+  // Creates circle with given radius
+  createCircle(latlong, radius) {
+    this.circle = L.circle(latlong, 1000, {
+      color: "#216477",
+      fillOpacity: 0.5,
+      weight: 1,
+    }).addTo(this.map);
+    //addTo(this.map)
+  }
 
-    // Edits circle
-    editCircle(radius){
-        this.circle.setRadius(radius)
-    }
+  // Edits circle
+  editCircle(radius) {
+    this.circle.setRadius(radius);
+  }
 
-    // Deletes all layers on map related to showing users location
-    deleteLocation(){
-        this.map.removeLayer(this.circle)
-        this.map.removeLayer(this.lmarker)
-    }
+  // Deletes all layers on map related to showing users location
+  deleteLocation() {
+    this.map.removeLayer(this.circle);
+    this.map.removeLayer(this.lmarker);
+  }
 
-    zoom(loc, zoom_lvl){
-        this.map.flyTo(loc, zoom_lvl)
-    }
+  zoom(loc, zoom_lvl) {
+    this.map.flyTo(loc, zoom_lvl);
+  }
 
-    // Get layerGroup object
-    get getLayerGroup(){
-        return this.layerGroup
-    }
+  // Get layerGroup object
+  get getLayerGroup() {
+    return this.layerGroup;
+  }
 
-    // Get map object
-    get Map(){
-        return this.map
-    }
+  // Get map object
+  get Map() {
+    return this.map;
+  }
 }
