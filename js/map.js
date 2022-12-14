f80215be669a7a29ff19ec677e02fd9e7669a83f
RauliYoung <tristan.ellenberg@gmail.com>
2022-12-14 10:10:57 +0200

    sisennetty koodia

--- js/map.js
+++ js/map.js
@@ -1,15 +1,10 @@
-'use strict';
+"use strict";
 
 // Longitude & Latitude of Helsinki
-let helsinki_coordinates = [60.192059, 24.945831]
+let helsinki_coordinates = [60.192059, 24.945831];
 
 // Map object initalization with zoom location set to Helsinki
 let map = new Map(helsinki_coordinates);
 
 // Map creation
-map.createMap("map")
-
-
-
-
-
+map.createMap("map");
